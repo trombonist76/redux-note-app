e8c59e33aa4a47581af139f6d993f31d4e20efd7
trombonist76 <mahmut.hg@gmail.com>
2022-05-18 00:16:17 +0300

    "First Commit"

---
+++ src/redux/notesSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from 'nanoid'
+
+const notesSlice = createSlice({
+    name: "notes",
+    initialState: {
+        items: [],
+        searchQuery: "",
+
+    },
+    reducers: {
+        addNote: (state, action) => {
+            state.items.push({
+                "id": nanoid(),
+                "note": action.payload.note,
+                "color": action.payload.color,
+            })
+        },
+        
+        search: (state, action) => {
+            state.searchQuery = action.payload
+        }
+    }
+
+})
+
+export const {addNote, search} = notesSlice.actions
+export default notesSlice.reducer
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import notesSlice from "./notesSlice";
+
+export const store = configureStore({
+    reducer : {
+        notes: notesSlice
+    }
+})
+
